827752d44af10a4f8ae8eab3ba94142d10ff448c
Renato Souza <reegomescel@gmail.com>
2019-11-08 17:24:16 -0300

    The end of the fucking world - Quem quiser farmar gaupoints, essa é a chance.

---
+++ Sentinel/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GauPoints
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TwitchChatBot bot = new TwitchChatBot();
+            bot.Connect();
+
+            Console.ReadLine();
+
+            bot.Disconnect();
+        }
+    }
+}
---
+++ Sentinel/TwitchChatBot.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using TwitchLib.Client.Models;
+using TwitchLib.Api;
+using TwitchLib.Client;
+using TwitchLib.Client.Events;
+using TwitchLib.Api.V5.Models.Users;
+
+namespace GauPoints
+{
+    internal class TwitchChatBot
+    {
+        ConnectionCredentials credentials = new ConnectionCredentials(Config.Default.BotUsername, Config.Default.BotToken);
+        TwitchClient client;
+        TwitchAPI api = new TwitchAPI();
+        
+        public TwitchChatBot()
+        {
+
+        }
+
+        internal void Connect()
+        {
+            Console.WriteLine("Connecting");
+            client = new TwitchClient();
+
+            client.Initialize(credentials, Config.Default.ChannelName);
+            client.OnLog += Client_OnLog;
+            client.OnConnectionError += Client_OnConnectionError;
+            // Until here, thats all ok
+
+            // 
+            client.OnMessageReceived += Client_OnMessageReceived;
+            //
+
+            client.Connect();
+
+            api.Settings.ClientId = Config.Default.ClientID;
+        }
+
+        private void Client_OnMessageReceived(object sender, OnMessageReceivedArgs e)
+        {
+            if (e.ChatMessage.Message != null)
+            {
+                Console.WriteLine("Teste");
+            } 
+        }
+
+        TimeSpan? GetUpTime()
+        {
+            string userId = GetUserId(Config.Default.ChannelName);
+            if(userId == null)
+            {
+                return null;
+            }
+
+            return api.V5.Streams.GetUptimeAsync(userId).Result;
+        }
+
+        string GetUserId(string username)
+        {
+            User[] userList = api.V5.Users.GetUserByNameAsync(username).Result.Matches;
+            if(userList == null || userList.Length == 0)
+            {
+                return null;
+            }
+            return userList[0].Id;
+        }
+
+        private void Client_OnLog(object sender, OnLogArgs e)
+        {
+            Console.WriteLine(e.Data);
+        }
+        private void Client_OnConnectionError(object sender, OnConnectionErrorArgs e)
+        {
+            Console.WriteLine($"Erro! : {e.Error}");
+        }
+
+        internal void Disconnect()
+        {
+            Console.WriteLine("Disconnecting");
+        }
+    }
+}
